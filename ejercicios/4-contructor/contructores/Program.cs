c97859582d9bffc16b94adb087e08656a049f6ef
marlonCruz <mrcruzm@unah.hn>
2021-03-08 21:53:48 -0600

    agregando programa contructores

---
+++ ejercicios/4-contructor/contructores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04_Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre = "cruz";
+            a.SegundoNombre = "marlon";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Walter";
+            b.SegundoNombre = "Augusto";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Juana";
+            c.SegundoNombre = "Maria";
+
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+        }
+    }
+}
