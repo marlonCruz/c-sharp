72146ab481f213bca7994ce4140963b7c6c3e982
marlonCruz <mrcruzm@unah.hn>
2021-03-15 22:15:47 -0600

    afregando ejercicio listados

---
+++ ejercicios/8-listados/listados/Alumno.cs
@@ -0,0 +1,11 @@
+clase  pública Alumno
+{
+    public  int  Id { obtener ; establecer ; }
+     cadena  pública Nombre { get ; establecer ; }
+
+    pública  alumno ( int  ID , cadena de  nombre )
+    {
+        Id  =  id ;
+        Nombre  =  nombre ;
+    }
+}
---
+++ ejercicios/8-listados/listados/Program.cs
@@ -0,0 +1,45 @@
+﻿usando el  sistema ;
+utilizando  System . Colecciones . Genérico ;
+
+ listados de espacios de nombres
+{
+     programa de clase
+    {
+        static  void  Main ( cadena [] argumentos )
+        {
+            Lista < cadena > nombres  =  nueva  Lista < cadena > ();
+            nombres . Agregar ( " Juan " );
+            nombres . Agregar ( " Pedro " );
+            nombres . Agregar ( " María " );
+
+            foreach ( var  nombre  en  nombres )
+            {
+                Consola . WriteLine ( nombre );
+            }
+
+            Lista < int > numeros  =  nueva  Lista < int > ();
+            numeros . Agregar ( 10 );
+            numeros . Agregar ( 1 );
+            numeros . Agregar ( 5 );
+
+            foreach ( var  n  en  numeros )
+            {
+                Consola . WriteLine ( n );
+            }
+
+            Lista < alumno > alumnos  =  nueva  lista < alumno > ();
+            Alumno  un  =  nuevo  alumno ( 1 , " Pedro " );
+            Alumno  b  =  nuevo  alumno ( 2 , " Maria " );
+            Alumno  c  =  nuevo  alumno ( 3 , " Juan " );
+
+            alumnos . Agregue ( a );
+            alumnos . Agregue ( b );
+            alumnos . Agregar ( c );
+
+            foreach ( var  alumno  en  alumnos )
+            {
+                Consola . WriteLine ( alumno . Id );
+            }
+        }
+    }
+}
